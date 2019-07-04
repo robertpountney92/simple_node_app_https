f7a6daf4d386193de337a44f83746763dda8d349
robertpountney92 <robertpountney92@gmail.com>
2019-07-04 13:02:13 +0100

    updated ports to 80

--- app/index.js
+++ app/index.js
@@ -1,13 +1,28 @@
 //Load express module with `require` directive
 var express = require('express')
+
+const https = require('https')
 var app = express()
 
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World!!!!!')
 })
 
+// const fs = require('fs')
+
+// //...
+
+// https.createServer({
+//   key: fs.readFileSync('server.key'),
+//   cert: fs.readFileSync('server.cert')
+// }, app).listen(8081, () => {
+//   console.log('Listening...')
+// })
+
+
+
 //Launch listening server on port 8081
-app.listen(8081, function () {
+app.listen(80, function () {
   console.log('app listening on port 80!')
 })
